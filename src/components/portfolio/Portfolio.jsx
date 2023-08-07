a2215cb321f7bb313d9a04f2d1fc3a9cd2f2b5eb
yash-mali <89188468+yash-mali@users.noreply.github.com>
2023-08-08 00:42:35 +0530

    Update Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,7 +5,7 @@ import IMG2 from "../../assets/sm.jpg";
 import IMG3 from "../../assets/earthly.png";
 import IMG4 from "../../assets/pm.jpg";
 import IMG5 from "../../assets/web.png";
-import IMG6 from "../../assets/Project4.jpg";
+import IMG6 from "../../assets/ctd.png";
 import React from "react";
 
 //Portfolio function
@@ -16,9 +16,19 @@ const Portfolio = () => {
       title: "Credenz'23",
       img: IMG1,
       description:
-        "An educational website is a huge interactive platform to present various information for people.",
-      technologies: "React Js | Django",
+        "Credenz, organized by the PICT IEEE Student Branch, is an annual technical festival that has become one of Pune's premier technical events.",
+      technologies: "ReactJs | Django",
       link: " https://credenz.in",
+      github: "",
+    },
+    {
+      id: 6,
+      title: "Credenz Tech Dayz",
+      img: IMG6,
+      description:
+        "An intra-college annual techfest  where the brightest minds of PICT compete in technical as well as non-technical events.",
+      technologies: "ReactJS | Django",
+      link: "https://ctd.pictieee.in",
       github: "",
     },
     {
@@ -26,8 +36,8 @@ const Portfolio = () => {
       title: "Project Mangement",
       img: IMG4,
       description:
-        "The COVID Tracking Project collects and publishes the most complete testing data available for all areas of the world.",
-      technologies: "React | Redux",
+        "Application that allows users to manage and collaborate on various project.",
+      technologies: "ReactJS | Firebase ",
       link: "https://project-management-29080.web.app/",
       github: "",
     },
@@ -35,8 +45,8 @@ const Portfolio = () => {
       id: 3,
       title: "Student Mangement",
       img: IMG2,
-      description: "This site is a kind of social media platform. You can register and share your posts.",
-      technologies: "React | Redux",
+      description: "Application that allows user to manage data related to students.",
+      technologies: "ReactJS | Firebase ",
       link: "https://student-portal-dde8a.web.app/",
       github: "",
     },
@@ -46,7 +56,7 @@ const Portfolio = () => {
       img: IMG3,
       description:
         "A dedicated, standalone web page built for specific campaigns and target audiences.",
-      technologies: "Html | CSS | JavaScript | Next Js",
+      technologies: "ReactJs",
       link: "https://earthly-sustainable.netlify.app/",
       github: "",
     },
@@ -55,27 +65,18 @@ const Portfolio = () => {
       title: "Fashion Ecommerce website",
       img: IMG5,
       description:
-        "For when you need a fast funny joke, here are some short jokes to get anyone giggling.",
-      technologies: "Html | Styled-components | Typescript",
+        "Online Shopping Site for Fashion & Lifestyle",
+      technologies: "Html | CSS | Javascript",
       link: "https://fashionbeginhere.netlify.app",
       github: "",
     },
-    // {
-    //   id: 6,
-    //   title: "Fs Poster Website",
-    //   img: IMG6,
-    //   description:
-    //     "Real-world group project which is still in progress and will provide educational platform for future young developers",
-    //   technologies: "Html | Scss | Javascript",
-    //   link: "https://fs-poster-project.vercel.app/",
-    //   github: "",
-    // },
+  
   ];
 
   return (
     <section id="portfolio">
       <h5>My Recent Work</h5>
-      <h2>Portfolio</h2>
+      <h2>Projects</h2>
 
       <div className="container portfolio__container">
         {soloProjects.map((pro) => (
